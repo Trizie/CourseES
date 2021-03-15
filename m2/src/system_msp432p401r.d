b612af2c7f2c3427a108005d76559db090aa3f17
Patrizia Stutzenstein <patrizia.stutzenstein@gmx.at>
2021-03-15 03:35:52 -0600

    C1M2 with all subfolders

---
+++ m2/src/system_msp432p401r.d
@@ -0,0 +1,19 @@
+system_msp432p401r.o: system_msp432p401r.c msp432p401r.h \
+ msp_compatibility.h core_cm4.h core_cmInstr.h cmsis_gcc.h core_cmFunc.h \
+ core_cmSimd.h system_msp432p401r.h
+
+msp432p401r.h:
+
+msp_compatibility.h:
+
+core_cm4.h:
+
+core_cmInstr.h:
+
+cmsis_gcc.h:
+
+core_cmFunc.h:
+
+core_cmSimd.h:
+
+system_msp432p401r.h:
